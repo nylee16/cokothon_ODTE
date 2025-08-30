2fb40082d6f23d7f5286cf6c1eb77b5df9ef25e2
luckyisjelly <assignment89@naver.com>
2025-08-30 22:33:43 +0900

    feat: vote 오류 수정

--- topicurator/src/main/java/com/odte/topicurator/repository/ProsnconsRepository.java
+++ topicurator/src/main/java/com/odte/topicurator/repository/ProsnconsRepository.java
@@ -4,4 +4,5 @@ import com.odte.topicurator.entity.Prosncons;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProsnconsRepository extends JpaRepository<Prosncons, Long> {
+    java.util.Optional<Prosncons> findByNewsId(Long newsId);
 }
