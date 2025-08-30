4668316aba35a7c76c6b680b009c87e15425ae4b
dmseong <149465698+dmseong@users.noreply.github.com>
2025-08-30 17:46:59 +0900

    프로젝트 세팅

---
+++ topicurator/src/main/java/com/odte/topicurator/TopicuratorApplication.java
@@ -0,0 +1,13 @@
+package com.odte.topicurator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicuratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicuratorApplication.class, args);
+	}
+
+}
---
+++ topicurator/src/test/java/com/odte/topicurator/TopicuratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.odte.topicurator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicuratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
